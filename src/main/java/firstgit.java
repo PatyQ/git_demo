6cdf64b5d7cb53c7685d9ba70dc42a7e880cc43f
PatyQ <qxy136181378@gmail.com>
2020-03-27 20:51:53 +0800

    删除了主分支中的s2信息并重新提交

--- src/main/java/firstgit.java
+++ src/main/java/firstgit.java
@@ -1,5 +1,5 @@
 public class firstgit {
     String s = "第一个git---master少的情况将多的domo合并过来+从" +
             "主分支创建了分支3 并且与主分支冲突";
-    String s2 = "master分支";
+    String s2 = "我删除了主分支中的s2信息并重新提交-demo3分支";
 }
