9673e1908344d98fd95113e22f8f8dbd4ee608c5
PatyQ <qxy136181378@gmail.com>
2020-03-27 13:05:37 +0800

    Revert "新建一个demo2分支"
    This reverts commit 2d37f0ee

--- src/main/java/firstgit.java
+++ src/main/java/firstgit.java
@@ -1,3 +1,7 @@
 public class firstgit {
-    String s = "第一个git-在提交后有冲突的情况下切换分支";
+    String s = "第一个git";
+
+    String s1 = "在demo1中修改并提交";//提交到仓库中后不会同步过去
+
+    String s2 = "将demo2中的分支合并到master中";
 }
