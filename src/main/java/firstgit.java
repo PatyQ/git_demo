e32ad1f24224ab9a27c390704c454b8063c782c0
PatyQ <qxy136181378@gmail.com>
2020-03-27 21:11:11 +0800

    在demo3中修改了master的信息并提交

--- src/main/java/firstgit.java
+++ src/main/java/firstgit.java
@@ -1,5 +1,5 @@
 public class firstgit {
     String s = "第一个git---master少的情况将多的domo合并过来+从" +
             "主分支创建了分支3 并且与主分支冲突";
-    String s2 = "我删除了demo3分支的s2并重新提交";
+    String s2 = "我删除了demo3分支的s2并重新提交-在demo3中修改了master的信息并提交";
 }
